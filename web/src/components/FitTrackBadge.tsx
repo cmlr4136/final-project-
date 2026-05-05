9e8dc511b687c2f09bf32db6855d69db09828469
cmlr4136 <cmlr4136@leeds.ac.uk>
2026-05-05 20:03:57 +0100

    Fully functional groupchats with examples and banner + badge update

---
+++ web/src/components/FitTrackBadge.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function FitTrackBadge({ className = "w-10 h-10" }: { className?: string }) {
+  return (
+    <svg 
+      viewBox="0 0 100 100" 
+      fill="none" 
+      xmlns="http://www.w3.org/2000/svg" 
+      className={className}
+    >
+      {/* Bold, clean black 'F' matching your image */}
+      <path d="M25 15H80V32H45V46H72V63H45V90H25V15Z" fill="#111827"/>
+    </svg>
+  );
+}
